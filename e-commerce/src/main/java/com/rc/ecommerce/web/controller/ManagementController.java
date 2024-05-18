f6429122b569fcfd2b86a1c1da8872e553aeba19
ruwanch <ruwanch@lbfinance.lk>
2024-05-18 16:20:03 +0530

    project minor changes

--- e-commerce/src/main/java/com/rc/ecommerce/web/controller/ManagementController.java
+++ e-commerce/src/main/java/com/rc/ecommerce/web/controller/ManagementController.java
@@ -1,31 +1,10 @@
 package com.rc.ecommerce.web.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/management")
-@Tag(name = "Management")
 public class ManagementController {
-
-
-    @Operation(
-            description = "Get endpoint for manager",
-            summary = "This is a summary for management get endpoint",
-            responses = {
-                    @ApiResponse(
-                            description = "Success",
-                            responseCode = "200"
-                    ),
-                    @ApiResponse(
-                            description = "Unauthorized / Invalid Token",
-                            responseCode = "403"
-                    )
-            }
-
-    )
     @GetMapping
     public String get() {
         return "GET:: management controller";
