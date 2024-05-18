aded74c8b61d45b266a78546c654541d871d825d
ruwanch <ruwanch@lbfinance.lk>
2024-05-18 16:17:29 +0530

    security config minor changes

--- e-commerce/src/main/java/com/rc/ecommerce/auditing/ApplicationAuditAware.java
+++ e-commerce/src/main/java/com/rc/ecommerce/auditing/ApplicationAuditAware.java
@@ -2,6 +2,7 @@ package com.rc.ecommerce.auditing;
 
 import com.rc.ecommerce.domain.User;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,6 +11,7 @@ import java.util.Optional;
 
 public class ApplicationAuditAware implements AuditorAware<Integer> {
     @Override
+    @NonNull
     public Optional<Integer> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
